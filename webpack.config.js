19e6d79eeb9b85a2aa3117c1571a0c2dc8877417
Borja Álvarez Piñeiro <borjaalvarez_18@hotmail.com>
2024-10-05 16:59:22 +0200

    Ts: Add suport to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,25 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname,"dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   mode: "development",
-  devServer:{
+  devServer: {
     static: "./dist"
   },
   module: {
     rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: "/node_modules/",
-            use: {
-                loader: "babel-loader"
-            }
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: "/node_modules/",
+        use: {
+          loader: "babel-loader"
         }
+      }
     ]
   },
-  resolve:{
-    extensions: [".js", ".jsx"]
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   }
 };
